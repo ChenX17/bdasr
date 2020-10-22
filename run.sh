9d4768134d55c9493192a6670fca7a8fc27d4cb3
ChenX17 <chenxi50@lenovo.com>
2020-10-22 21:05:34 +0800

    rm run.sh

--- run.sh
+++
@@ -1,3 +0,0 @@
-python eeasr/train_sb_asr.py --config /datadisk/model/aishell1/config/final_bd_8_4_1024units_exp1.yaml
-python eeasr/train_sb_asr.py --config /datadisk/model/aishell1/config/final_bd_8_4_1024units_exp2.yaml
-python eeasr/train_sb_asr.py --config /datadisk/model/aishell1/config/final_bd_8_4_1024units_exp3.yaml
