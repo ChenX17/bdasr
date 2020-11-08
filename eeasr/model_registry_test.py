fd892d2d64f5ed4efd7e967e5d4f0e20ab17affb
ChenX17 <chenxi50@lenovo.com>
2020-11-08 15:30:02 +0800

    remove eeasr remove scripts

--- eeasr/model_registry_test.py
+++
@@ -1,9 +0,0 @@
-from eeasr import model_registry
-
-@model_registry.RegisterSingleTaskModel
-class DummyModel(object):
-  def __init__():
-    pass
-
-
-print(model_registry.GetAllRegisteredClasses())
